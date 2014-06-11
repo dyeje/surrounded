78e7d16f5ab54523fc12619e63c4fbd398baa4e0
Jeremy Dye <jeremy.dye@parelio.com>
2014-06-11 01:41:06 -0400

    Sorta working moving enemies.

--- surrounded.js
+++ surrounded.js
@@ -6,11 +6,94 @@ var x = 300;
 var y = 300;
 var WIDTH = 600;
 var HEIGHT = 600;
+var enemies = [];
+var player = {}
+
+player.draw = function () {
+  rect(x, y, 10, 10);
+}
+
+var cardinalDirections = {
+  north: 1,
+  south: 2,
+  east: 3,
+  west: 4
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomCardinalDirection() {
+  return getRandomInt(1, 4);
+}
+
+function randomSpeed(direction) {
+  var speed = getRandomInt(10, 30);
+  if (direction === cardinalDirections.south || cardinalDirections.west) {
+    return speed * -1;
+  } else {
+    return speed;
+  }
+}
+
+function randomSize() {
+  return getRandomInt(10, 40);
+}
+
+function randomStartingCoordinates(cardinalDirection) {
+  if (cardinalDirection === cardinalDirections.north) {
+    return {x: getRandomInt(0, WIDTH), y: 0};
+  } else if (cardinalDirection === cardinalDirections.south) {
+    return {x: getRandomInt(0, WIDTH), y: HEIGHT};
+  } else if (cardinalDirection === cardinalDirections.east) {
+    return {x: 0, y: getRandomInt(0, HEIGHT)};
+  } else if (cardinalDirection === cardinalDirections.west) {
+    return {x: WIDTH, y: getRandomInt(0, HEIGHT)};
+  }
+} 
+
+function initializeEnemy(id) {
+  var enemy = {
+    id: id,
+    direction: randomCardinalDirection(),
+    size: randomSize()
+  }
+
+  var startingCoordinates = randomStartingCoordinates(enemy.direction);
+
+  enemy.x = startingCoordinates.x;
+  enemy.y = startingCoordinates.y;
+  enemy.speed = randomSpeed(enemy.direction);
+
+  return enemy;
+}
+
+function enemyMove(enemy) {
+  if (enemy.direction === cardinalDirections.east || enemy.direction === cardinalDirections.west) {
+    enemy.x = enemy.x + enemy.speed;
+  } else {
+    enemy.y = enemy.y + enemy.speed;
+  }
+
+  if (enemy.x < 0 || enemy.x > WIDTH || enemy.y < 0 || enemy.y > HEIGHT) {
+    enemies[enemy.id] = initializeEnemy();
+  }
+}
+
+function enemyDraw(enemy) {
+  rect(enemy.x, enemy.y, enemy.size, enemy.size);
+} 
+
+for (var i = 0; i < 20; i++) {
+  enemies.push(initializeEnemy(i));
+} 
 
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  return setInterval(draw, 10);
+  setInterval(drawPlayer, 10);
+  setInterval(drawEnemies, 500);
 }
 
 function rect(x, y, w, h) {
@@ -26,9 +109,17 @@ function clear() {
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
 }
 
-function draw() {
+function drawPlayer() {
+  player.draw();
+}
+
+function drawEnemies() {
   clear();
-  rect(x, y, 10, 10);
+  player.draw();
+  for (i = 0; i < enemies.length; ++i) {
+    enemyMove(enemies[i]);
+    enemyDraw(enemies[i]);
+  }
 }
 
 function keyDown(e) {
